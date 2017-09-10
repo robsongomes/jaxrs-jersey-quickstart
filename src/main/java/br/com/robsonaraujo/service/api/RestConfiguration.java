d3d10032adb1ae625882d5d3b8c4792522baf3ec
robson <nosbor84@gmail.com>
2017-09-09 22:06:31 -0300

    Initial commit

---
+++ src/main/java/br/com/robsonaraujo/service/api/RestConfiguration.java
@@ -0,0 +1,9 @@
+package br.com.robsonaraujo.service.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/services")
+public class RestConfiguration extends Application {
+	
+}
